79fc1b4ecb9ae6b03ce82800d973f7a162939e5b
Javier Montero <montero.javier@opendeusto.es>
2025-11-04 21:04:57 +0100

    Ventan1

--- src/gui/Ventana1.java
+++ src/gui/Ventana1.java
@@ -1,20 +1,34 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Ventana1 extends JFrame{
-		private JPanel pcentro, pSur;
+		private JPanel pCentro, pSur;
 		private JButton btnSeguir;
-		private JLabel lbl1;
+		private JLabel lblImagen;
 		
 		public Ventana1 () {
 			
+			pCentro = new JPanel();
+			
+			
+			setExtendedState(JFrame.MAXIMIZED_BOTH);
+			ImageIcon icono = new ImageIcon(getClass().getResource("/images/ImagenTrans.png"));
+			Image img = icono.getImage().getScaledInstance(300, 200, Image.SCALE_SMOOTH);
+			icono = new ImageIcon(img);
+			
+			lblImagen = new JLabel(icono);
+			pCentro.add(lblImagen);
 		}
+	
+		
+}
 
-	}
 
---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import gui.Ventana1;
+
+public class Main {
+	public static void main(String[] arg) {
+		
+		new Ventana1();
+	}
+
+}
