cf2dd5242435f9a063a1654bbead3f800ee941df
Javier Montero <montero.javier@opendeusto.es>
2025-11-04 20:01:19 +0100

    imagen

--- src/gui/Ventana1.java
+++ src/gui/Ventana1.java
@@ -1,5 +1,20 @@
 package gui;
 
-public class Ventana1 {
+import java.awt.BorderLayout;
 
-}
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Ventana1 extends JFrame{
+		private JPanel pcentro, pSur;
+		private JButton btnSeguir;
+		private JLabel lbl1;
+		
+		public Ventana1 () {
+			
+		}
+
+	}
+
