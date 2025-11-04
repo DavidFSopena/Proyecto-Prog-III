87e419ca535e05e1f4e2add0bfb9210308c7d9ca
Javier Montero <montero.javier@opendeusto.es>
2025-11-04 21:33:28 +0100

    imagen

--- src/gui/Ventana1.java
+++ src/gui/Ventana1.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -20,14 +22,27 @@ public class Ventana1 extends JFrame{
 			
 			
 			setExtendedState(JFrame.MAXIMIZED_BOTH);
-			ImageIcon icono = new ImageIcon(getClass().getResource("/images/ImagenTrans.png"));
-			Image img = icono.getImage().getScaledInstance(300, 200, Image.SCALE_SMOOTH);
-			icono = new ImageIcon(img);
+			setUndecorated(true);
 			
+			Color TurquesaC = new Color(96, 198, 194);
+			getContentPane().setBackground(TurquesaC);
+			
+			pCentro = new JPanel();
+			pCentro.setOpaque(true);
+			pCentro.setLayout(new GridLayout());
+			
+			ImageIcon ruta = new ImageIcon(getClass().getResource("/images/ImagenTrans.png"));
+			Image img = ruta.getImage().getScaledInstance(600, 500, Image.SCALE_SMOOTH);
+			ImageIcon icono = new ImageIcon(img);
 			lblImagen = new JLabel(icono);
+			
 			pCentro.add(lblImagen);
+			add(pCentro, BorderLayout.CENTER);
+			
+			setVisible(true);
 		}
-	
+		
+		
 		
 }
 
