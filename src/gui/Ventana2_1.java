40bc9b97aa4fe52fe7abd54f4e77d7ca3a55b19f
David Fernández-Sopeña <d.fernandezsopena@opendeusto.es>
2025-11-05 17:44:56 +0100

    Actualización logo y botones

--- src/gui/Ventana2_1.java
+++ src/gui/Ventana2_1.java
@@ -10,6 +10,9 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -20,19 +23,18 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class Ventana2_1 extends JFrame {
 	private JPanel pSur, pCentro, pCentroConLogo, pNorte;
 	private JLabel lblTitulo, lblNombre, lblContrasena;
-	private JButton btnIniciarSesion, btnRegistrarse, btnCancelar;
+	private JButton btnIniciarSesion, btnRegistrarse, btnVolver;
 	private JTextField txtNombre;
 	private JPasswordField txtContrasena;
 	
 	public Ventana2_1() {
-		setBounds(400,200,600,400);
-		setTitle("Registro e inicio de sesión");
+		setTitle("BilboBnB - Acceso");
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		setUndecorated(true);
 		
 		Color turquesa = new Color(96, 198, 194);
 		Color coral = new Color(255,102,102);
@@ -44,31 +46,39 @@ public class Ventana2_1 extends JFrame {
 		pSur = new JPanel();
 		pCentro = new JPanel();
 		pCentro.setLayout(new GridLayout(2,2,10,15));
-		pCentro.setBorder(BorderFactory.createEmptyBorder(250, 250, 250, 250));
+		pCentro.setBorder(BorderFactory.createEmptyBorder(12, 240, 240, 240));
 		pCentro.setBackground(turquesa);
 		pCentroConLogo = new JPanel();
 		pCentroConLogo.setBackground(turquesa);
+		pCentroConLogo.setLayout(new BorderLayout());
 		setLayout(new BorderLayout(0,20)); 
 		
 		//Cración de componentes
 		btnIniciarSesion = new JButton("INICIAR SESIÓN");
+		btnIniciarSesion.setForeground(coral);
+		btnIniciarSesion.setBackground(Color.WHITE);
+		//btnIniciarSesion.setBorder(new LineBorder(coral));
 		btnRegistrarse = new JButton("REGISTRARSE");
-		btnCancelar = new JButton("SALIR");
-		btnIniciarSesion.setBackground(coral);
-		btnRegistrarse.setBackground(coral);
-		btnCancelar.setBackground(coral);
+		btnRegistrarse.setForeground(coral);
+		btnRegistrarse.setBackground(Color.WHITE);
+		//btnRegistrarse.setBorder(new LineBorder(coral));
+		btnVolver = new JButton("VOLVER");
+		btnVolver.setForeground(coral);
+		btnVolver.setBackground(Color.WHITE);
+		//btnVolver.setBorder(new LineBorder(coral));
 		
 		lblTitulo = new JLabel("BIENVENIDO", JLabel.CENTER);
 		lblTitulo.setFont(new Font("Arial", Font.BOLD, 28));
 		
-		ImageIcon icono = new ImageIcon("/images/ImagenTrans.png");
-		Image imagenEscalada = icono.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH);
+		ImageIcon icono = new ImageIcon("resources/images/ImagenTrans1.png");
+		Image imagenEscalada = icono.getImage().getScaledInstance(320, 320, Image.SCALE_SMOOTH);
 		JLabel lblLogo = new JLabel(new ImageIcon(imagenEscalada));
 		lblLogo.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblLogo.setBorder(BorderFactory.createEmptyBorder(60, 0, 0, 0));
 		
-		lblNombre = new JLabel("Introduce tu nombre: ", JLabel.CENTER);
+		lblNombre = new JLabel("INTRODUCE TU NOMBRE: ", JLabel.CENTER);
 		lblNombre.setFont(new Font("Arial", Font.BOLD,20));
-		lblContrasena = new JLabel("Introduce tu contraseña: ", JLabel.CENTER);
+		lblContrasena = new JLabel("INTRODUCE TU CONTRASEÑA: ", JLabel.CENTER);
 		lblContrasena.setFont(new Font("Arial", Font.BOLD,20));
 		txtNombre = new JTextField(1);
 		txtNombre.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -82,18 +92,18 @@ public class Ventana2_1 extends JFrame {
 		getContentPane().add(pNorte, BorderLayout.NORTH);
 		getContentPane().add(pCentroConLogo,BorderLayout.CENTER);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
-		getContentPane().add(pCentro, BorderLayout.CENTER);
 		
 		//Añadir componentes a los paneles
 		pNorte.add(lblTitulo);
-		pCentroConLogo.add(lblLogo);
+		pCentroConLogo.add(lblLogo, BorderLayout.NORTH);
 		pCentro.add(lblNombre);
 		pCentro.add(txtNombre);
 		pCentro.add(lblContrasena);
 		pCentro.add(txtContrasena);
+		pCentroConLogo.add(pCentro, BorderLayout.CENTER);
 		pSur.add(btnIniciarSesion);
 		pSur.add(btnRegistrarse);
-		pSur.add(btnCancelar);
+		pSur.add(btnVolver);
 
 		//Listeners de los componentes
 		btnIniciarSesion.addActionListener((e) -> {
@@ -111,15 +121,58 @@ public class Ventana2_1 extends JFrame {
 		
 		
 		btnRegistrarse.addActionListener((e) -> {
-			Ventana2_1.this.setVisible(false);
 			Ventana2_2 nuevaVentana = new Ventana2_2();
-			nuevaVentana.setVisible(true);
+			nuevaVentana.setAlwaysOnTop(true);
+			nuevaVentana.setLocationRelativeTo(Ventana2_1.this);
 		});
 		
-		btnCancelar.addActionListener((e) -> {
+		btnVolver.addActionListener((e) -> {
 			Ventana2_1.this.setVisible(false);
 			Ventana1 nuevaVentana = new Ventana1();
 			nuevaVentana.setVisible(true);
+		});
+		
+		btnIniciarSesion.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnIniciarSesion.setForeground(Color.WHITE);
+				btnIniciarSesion.setBackground(coral);	
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnIniciarSesion.setForeground(coral);
+				btnIniciarSesion.setBackground(Color.WHITE);
+			}
+		});
+		
+		btnRegistrarse.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnRegistrarse.setForeground(Color.WHITE);
+				btnRegistrarse.setBackground(coral);
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnRegistrarse.setForeground(coral);
+				btnRegistrarse.setBackground(Color.WHITE);
+			}
+		});
+		
+		btnVolver.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnVolver.setForeground(Color.WHITE);
+				btnVolver.setBackground(coral);
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnVolver.setForeground(coral);
+				btnVolver.setBackground(Color.WHITE);
+			}
 		});
 		
 		setVisible(true);
